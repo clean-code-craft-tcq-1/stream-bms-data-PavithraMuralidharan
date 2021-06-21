470ae2d3555655e9a7cea59285e628c69e12a66a
aswinmath94 <49982968+aswinmath94@users.noreply.github.com>
2021-06-21 12:21:51 +0530

    Update bms_stream_formatter.py

--- receiver/bms_stream_formatter.py
+++ receiver/bms_stream_formatter.py
@@ -6,12 +6,16 @@ import pandas as pd
 def bms_stream_formatting():
     data = sys.stdin.readlines()
     input_data = ''
-
+    
+    #get the streaming data from java console output in the pipeline
     for line in data:
         if 'temperature' in line:
             input_data = line
             
+    #convert bms data stream string to list of dictionaries
     formated_data_stream = ast.literal_eval(input_data)
+    
+    #convert the list of dictionaries to a dataframe and return bms parameter lists
     bms_dataframe = pd.DataFrame(formated_data_stream)
     if bms_dataframe.size !=0:
         bms_temperature_list = bms_dataframe['temperature'].values.tolist()
