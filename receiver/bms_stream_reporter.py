aa83d9b755e4475d7321f977575142fb55dd5fea
aswinmath94 <49982968+aswinmath94@users.noreply.github.com>
2021-06-21 12:28:03 +0530

    Update bms_stream_reporter.py

--- receiver/bms_stream_reporter.py
+++ receiver/bms_stream_reporter.py
@@ -19,20 +19,20 @@ def displayOutput(bms_data):
 
     print('Max Temperature:', bms_param_max(bms_temperature_list))
     print('Min Temperature:', bms_param_min(bms_temperature_list))
-    print('Moving average of last five values of temperature:', moving_average_last_5(bms_temperature_list))
+    print('Moving average of last five values of temperature:', bms_moving_average_last_5(bms_temperature_list))
 
     print('Max SOC:', bms_param_max(bms_soc_list))
     print('Min SOC:', bms_param_min(bms_soc_list))
-    print('Moving average of last five values of chargeRate:', moving_average_last_5(bms_soc_list))
+    print('Moving average of last five values of chargeRate:', bms_moving_average_last_5(bms_soc_list))
 
     print('Max chargeRate:', bms_param_max(bms_chargeRate_list))
     print('Min chargeRate:', bms_param_min(bms_chargeRate_list))
-    print('Moving average of last five values of chargeRate:', moving_average_last_5(bms_chargeRate_list))
+    print('Moving average of last five values of chargeRate:', bms_moving_average_last_5(bms_chargeRate_list))
     return 'Success'
 
 
 # returns moving average of last 5 values in bms parameter list
-def moving_average_last_5(bms_param_data):
+def bms_moving_average_last_5(bms_param_data):
     length = len(bms_param_data)
     if length >= 5:
         moving_average = round((sum(bms_param_data[-5:])/5),3)
